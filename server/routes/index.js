3a6b60d9556d7ca0e2c238176e0708b301b69a39
Adam Foosaner <nucat176@gmail.com>
2018-09-19 16:33:13 -0700

    Adds more basic routes for the three main entities

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,19 @@ module.exports = (app) => {
     app.get('/api', (req, res) => res.status(200).send({
         message: 'Welcome to the nCent Hybrid Meta-App API!'
     }));
+
+    app.get('/api/challenges', challengesController.list);
+    app.get('/api/challenges/:uuid', challengesController.retrieve);
+    app.post('/api/challenges', challengesController.create);
+    app.patch('/api/challenges/:uuid', challengesController.update);
+
+    app.get('/api/tasks', tasksController.list);
+    app.get('/api/tasks/:uuid', tasksController.retrieve);
+    app.post('/api/tasks', tasksController.create);
+    app.patch('/api/tasks/:uuid', tasksController.update);
+
+    app.get('/api/users', usersController.list);
+    app.get('/api/users/:uuid', usersController.retrieve);
+    app.post('/api/users', usersController.create);
+    app.patch('/api/users/:uuid', usersController.update);
 };
