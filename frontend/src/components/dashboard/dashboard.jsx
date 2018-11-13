7a9e716fe12671e87c6ac1521cd61978914adc64
Adam Foosaner <nucat176@gmail.com>
2018-11-12 16:21:41 -0800

    fixes styling on nav bar on dashboard

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -277,7 +277,11 @@ class Dashboard extends React.Component {
         return (
             <a
                 title="Sponsor"
-                className="nav-item create-payment active"
+                className={
+                    this.state.formType === "Sponsor"
+                        ? "nav-item active"
+                        : "nav-item"
+                }
                 onClick={this.handleInput("formType")}
             >
                 <span className="button-text">Sponsor</span>
@@ -386,21 +390,7 @@ class Dashboard extends React.Component {
                                         }
                                         onClick={this.handleInput("formType")}
                                     >
-                                        {" "}
-                                        <div id="ember1183" className="inline-svg ">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="22"
-                                                height="22"
-                                                viewBox="0 0 22 22"
-                                            >
-                                                <path
-                                                    d="M11 0c6.072 0 11 4.928 11 11s-4.928 11-11 11S0 17.072 0 11 4.928 0 11 0zm0 2c-4.968 0-9 4.032-9 9s4.032 9 9 9 9-4.032 9-9-4.032-9-9-9zm2.303 7.2a.288.288 0 0 0 .403-.009l.552-.569a.296.296 0 0 0-.014-.426 4.341 4.341 0 0 0-1.478-.844l.173-.838a.292.292 0 0 0-.283-.354h-1.065a.29.29 0 0 0-.283.233l-.156.744c-1.416.072-2.616.79-2.616 2.265 0 1.277.993 1.824 2.041 2.202.993.379 1.518.52 1.518 1.052 0 .547-.525.87-1.297.87-.705 0-1.443-.237-2.015-.81a.284.284 0 0 0-.404-.002l-.593.594a.299.299 0 0 0 .002.423 3.9 3.9 0 0 0 1.714.97l-.162.776a.292.292 0 0 0 .28.355l1.067.008a.29.29 0 0 0 .285-.233l.154-.746c1.696-.106 2.734-1.043 2.734-2.416 0-1.262-1.034-1.795-2.29-2.23-.717-.266-1.338-.448-1.338-.995 0-.533.58-.744 1.159-.744.74 0 1.449.305 1.913.724z"
-                                                    fillRule="nonzero"
-                                                />
-                                            </svg>
-                                        </div>
-                                        <span className="nav-item-label">jobCents</span>
+                                        <span className="button-text">Wallet</span>
                                     </a>
                                     {/*<a*/}
                                     {/*title="Settings"*/}
@@ -437,21 +427,7 @@ class Dashboard extends React.Component {
                                         }
                                         onClick={this.handleInput("formType")}
                                     >
-                                        <div id="ember1207" className="inline-svg ">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="23"
-                                                height="22"
-                                                viewBox="0 0 23 22"
-                                            >
-                                                <g fill="#FFF" fillRule="evenodd">
-                                                    <path
-                                                        d="M11.5 22c-6.1 0-11-4.9-11-11s4.9-11 11-11 11 4.9 11 11-4.9 11-11 11zm0-20c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9z"/>
-                                                    <path d="M7 10h9v2H7z"/>
-                                                </g>
-                                            </svg>
-                                        </div>
-                                        <span className="nav-item-label">Sign Out</span>
+                                        <span className="button-text">Sign Out</span>
                                     </a>
                                     {this.sponsorChallengeButton()}
                                 </nav>
